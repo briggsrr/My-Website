f417c597f3738923272ff2a100e6b5dfc29f3db7
briggsr20 <bobbybriggs3@gmail.com>
2022-06-14 22:40:45 -0700

    start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Yo</h1>
+    <div>
+      <Navbar/>
     </div>
   );
 }
