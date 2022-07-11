200b7b371c1b303703936c5c8b5213ceec86250e
briggsrr <robert.briggs.3@icloud.com>
2022-07-11 13:08:20 -0700

    Fixed // warning

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -33,7 +33,7 @@ const Sidebar = ({isOpen, toggle}) => {
             </SidebarMenu>
             <SideBtnWrap>
                 <SidebarRoute to='/database-test' onClick={toggle}>
-                    //database
+                    database
                 </SidebarRoute>
             </SideBtnWrap>
         </SidebarWrapper>
