14f319da546829b7955017bf1da71e9391f07239
briggsrr <robert.briggs.3@icloud.com>
2022-07-20 16:18:43 -0700

    Database page cleanup

--- src/components/Database/DatabaseElements.js
+++ src/components/Database/DatabaseElements.js
@@ -7,7 +7,7 @@ export const Container = styled.div`
     z-index: 0;
     background: #0b192f;
 
-    border: 1px solid #fff;
+    
     
 `
 
@@ -16,33 +16,92 @@ export const HomeLink = styled(Link)`
     color: #64ffda;
     display: flex;
     cursor: pointer;
-
+    justify-content: center;
+    padding-top: 30px;
+    margin-bottom:350px;
+    text-decoration: none;
+    
 
     &:hover {
         color: #8892af;
         transition: 0.2s ease-in-out;
     }
+    
  
 `;
 
 export const NavbarContainer = styled.div`
-
-
+    height: 80px;
+    //display: flex;
+    //border: 1px solid #fff;
     
 `;
 
-export const ClickerContainer = styled.div`
+// export const ClickerContainer = styled.div`
+//     display: grid;
+//     border: 1px solid #fff;
+//     height: 400px;
+//     justify-content: center;
+
+    
+//     grid-template-columns: "a" 
+//                             "b";
     
     
 
 
 
-`;
+// `;
 
-export const DataButton = styled.button`
+// export const DataButton1 = styled.button`
+//     //display: flex;
+  
+//   align-items: center;
+//   height: 200px;
+//   width: 200px;
+//   border: 3px solid green;
+//   grid-area: a;
+  
+//   &:hover {
+//         transform: scale(1.03);
+//         transition: all 0.2s ease-in-out;
+//         cursor: pointer;
+//     }
+// &:active {
+  
+//   box-shadow: 0 5px #666;
+//   transform: translateY(10px);
+//   transition: all .2s ease-in-out;
+// }
+
     
-    
+  
+  
+
+// `;
+
+// export const DataButton2 = styled.button`
+// //display: flex;
+//   justify-content: center;
+
+//   height: 200px;
+//   border: 3px solid green;
+  
+//   width: 200px;
+
+//   &:hover {
+//         transform: scale(1.03);
+//         transition: all 0.2s ease-in-out;
+//         cursor: pointer;
+//     }
+//     &:active {
+  
+//   box-shadow: 0 5px #666;
+//   transform: translateY(10px);
+//   transition: all 0.2s ease-in-out;
+//   grid-area:b
+// }
 
 
 
-`;
+// `;
--- src/components/Database/index.js
+++ src/components/Database/index.js
@@ -2,29 +2,27 @@ import React from 'react'
 import { Container,
     HomeLink,
     NavbarContainer,
-    ClickerContainer,
-    DataButton
+
+
       
   
   } from './DatabaseElements'
 
+
+
+
+
+
+
 const DatabaseNavbar = () => {
   return (
+    
     <>
         <Container>
           <NavbarContainer>
             <HomeLink to="/">&#47;&#47;home </HomeLink>
 
-              <ClickerContainer>
- 
-                  <DataButton onClick={() => this.setData()}> Set Data </DataButton>
-                 
-                  <DataButton onClick={() => this.setData()}> Get Data </DataButton>
-                  
-                  
-
-              </ClickerContainer>
-
+  
       
           </NavbarContainer>
         </Container>
