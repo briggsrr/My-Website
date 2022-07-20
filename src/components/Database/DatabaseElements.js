8179b2a1adade9873e04b618fe4c6f9eafb5aa45
briggsrr <robert.briggs.3@icloud.com>
2022-07-19 18:39:49 -0700

    Cleaned for creation attempt for databasepage

--- src/components/Database/DatabaseElements.js
+++ src/components/Database/DatabaseElements.js
@@ -6,6 +6,8 @@ export const Container = styled.div`
     position:grid;
     z-index: 0;
     background: #0b192f;
+
+    border: 1px solid #fff;
     
 `
 
@@ -13,15 +15,6 @@ export const HomeLink = styled(Link)`
     width: 100%;
     color: #64ffda;
     display: flex;
-    text-decoration: none;
-    //border-bottom: 1px solid #64ffda;
-    /* padding: 0 2rem; */
-    align-items:center;
-    height: 100px;
-    //background-color: #10213e;
-    justify-content: center;
-    /* margin-left: 75px;
-    margin-top: 75px; */
     cursor: pointer;
 
 
@@ -35,26 +28,11 @@ export const HomeLink = styled(Link)`
 export const NavbarContainer = styled.div`
 
 
-    display: grid;
-    z-index: 1;
-    height: 300px;
-    width: 100%;
-    max-width: 1000px;
-    margin-right:auto;
-    margin-left: auto;
-    padding: 0 24px;
     
 `;
 
 export const ClickerContainer = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 200px;
-    position: relative;
-    border: 3px solid green;
-    grid-area: b;
+    
     
 
 
